3be9afd286d51144a403232ef7f703769417b9fb
Juan Silva <juan.silvawd@gmail.com>
2022-06-12 22:05:41 -0500

    more practice

--- src/scannerPractice.java
+++ src/scannerPractice.java
@@ -12,6 +12,26 @@ public class scannerPractice {
 
         String name = scan.next();
 
+        for(int i = 1; i <= 10; i++) { // <--+
+            if(i % 2 == 1) { //              |
+                continue; // ----------------+
+            }
+            System.out.println(i);
+
+            int caseSwitch = 1;
+
+            switch (caseSwitch) {
+                case 1:
+                    System.out.println("Case 1");
+                    // Fall through!
+                case 2:
+                    System.out.println("Case 2");
+                    break;
+                default:
+                    System.out.println("Default case");
+                    break;
+            }
+        }
 
     }
 }
