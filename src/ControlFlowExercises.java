c03453dc171f781b3efc825ecbc9931af259893d
Juan Silva <juan.silvawd@gmail.com>
2022-06-13 11:02:48 -0500

    today's update

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,76 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        // 1. A
+//        double i = 5;
+//        while (i <= 15) {
+//            System.out.print(i);
+//            i++;
+//        }
+//        int number = 0;
+//        do {
+//            System.out.println(number);
+//            number += 2;
+//        } while (number <= 100);
+//    }
+// 1. B
+//    int hundred = 100;
+//
+//    do{
+//        System.out.println(hundred);
+//        hundred -= 5;
+//    } while (hundred >= -10);
+//
+//        double combining = 2;
+//        do {
+//            System.out.println(combining);
+//            combining *= combining;
+//        } while (combining <= 1000000);
+
+
+//        for (int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+        //2. Fizzbuzz
+//        for (int a = 1; a <= 100; a++) {
+//            if (a % 15 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (a % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else if (a % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(a);
+//            }
+//        }
+        // 3. using a scanner feature to display table number / squared / cubed.
+//        Scanner tablePower = new Scanner((System.in);
+//        System.out.println("Number to Power Up");
+//        int numberEntered = tablePower.nextInt();
+//        String tableSquare = "Here is your Table up to 5";
+//        System.out.println(tableSquare);
+
+        //4. Letter Grades
+//        boolean gradeChoice = true;
+//
+//while(gradeChoice){
+//    System.out.println("Enter Grade (0-100");
+//    int grade = sc.nextInt();
+//    if (grade > 0 && grade = 59) {
+//        System.out.println("F");
+//    }else if (grade > 59 && grade <=66){
+//        System.out.println("D");
+//    }else if (grade > 66 && grade <= 79){
+//        System.out.println("C");
+//    }else if (grade > 79 && grade <= 87){
+//        System.out.println("B");
+//    }else if (grade > 87 && grade <=100){
+//        System.out.println("A");
+//    }
+//}
+//
+//    }
+    }
+}
---
+++ src/StringExercise.java
@@ -0,0 +1,35 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+        String Statement = "We don't need no education";
+        System.out.println(Statement);
+
+        System.out.println(Statement.replace( "education", "thought control"));
+
+
+    }
+}
+//    String message = "Hello";
+//System.out.println(message); // prints "Hello"
+//        message = message + " World!";
+//        System.out.println(message); // prints "Hello, World!"
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Continue? [y/N] ")
+//        String userInput = sc.next();
+//
+//// Don't do this!
+//        boolean confirmation = userInput == "y";
+//        String input = "Codeup Rocks!";
+//
+//        input.equals("codeup rocks!"); // false
+//        input.equals("Codeup Rocks!"); // true
+//
+//        input.equalsIgnoreCase("codeup rocks!"); // true
+//        input.equalsIgnoreCase("Codeup Rocks!"); // true
+//
+//        input.startsWith("codeup") // false
+//        input.startsWith("Codeup") // true
+//
+//        input.endsWith("rocks") // false
+//        input.endsWith("rocks!") // false
+//        input.endsWith("Rocks!") // true
