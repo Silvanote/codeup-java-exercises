9a4957f79954d6a87985c00f61456461906b9b0d
Juan Silva <juan.silvawd@gmail.com>
2022-06-12 22:04:19 -0500

    practice java

---
+++ src/scannerPractice.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class scannerPractice {
+
+    public static void main(String[] args) {
+
+        // Scanner is used to get input from user or to lines from a file.
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What's your first name?");
+
+        String name = scan.next();
+
+
+    }
+}
