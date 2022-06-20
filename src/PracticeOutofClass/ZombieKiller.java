3b0073e5bc310e1b1a2f2ffb6435d2b302491f9f
Juan Silva <juan.silvawd@gmail.com>
2022-06-19 21:29:55 -0500

    array

---
+++ src/PracticeOutofClass/ZombieKiller.java
@@ -0,0 +1,22 @@
+package PracticeOutofClass;
+
+public class ZombieKiller {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our Zombie Killer Program");
+
+String backpack [] = {"Shotgun", "Assault Rifle", "Sniper"};
+String zombies [] = {"Close-range zombie", "Mid-range zombie", "Long-range zombie"};
+int numbersZombiesHate[] = {4, 90, 70, 123, 12, 4, 261, 11};
+
+        System.out.println(numbersZombiesHate[6]);
+        System.out.println("Backpack items!");
+        System.out.println(backpack[0]);
+        System.out.println(backpack[1]);
+        System.out.println(backpack[2]);
+
+        System.out.println("These are the types of zombies");
+        System.out.println(zombies[0]);
+        System.out.println(zombies[1]);
+        System.out.println(zombies[2]);
+    }
+}
