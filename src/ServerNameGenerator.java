76d6adacc1e1e85ba41afbdbaa2f9f9faf79cc43
Juan Silva <juan.silvawd@gmail.com>
2022-06-22 16:55:44 -0500

    study

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,4 +1,22 @@
 public class ServerNameGenerator {
-    private static String[] adjectives = {"red", "strong", "light", "boldly", "yellow"};
-    private static String[] nouns = {"cup", "sign", "building", "car", "desk"};
+    static String[] adjectives = {"red", "strong", "light", "boldly", "yellow", "sharp", "dull", "quick", "slow", "tall"};
+    static String[] nouns = {"cup", "sign", "building", "car", "desk", "fan", "light", "bucket", "dog", "napkin"};
+
+    public static String RandomEl(String[] strings) {
+        int randomNum = (int) (Math.random() * strings.length);
+        return strings[randomNum];
+    }
+
+    public static String makeServerName(String[] strings1, String[] strings2){
+        String Nounslisted= RandomEl(strings1);
+        String Adjlisted = RandomEl(strings2);
+        String finalString = Nounslisted + "-" + Adjlisted;
+        return finalString;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Here is your server name: (adjective - noun)");
+        System.out.println(makeServerName(adjectives,nouns));
+    }
 }
+
