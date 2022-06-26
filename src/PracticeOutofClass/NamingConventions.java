38c108acb04562dca240f5b0e480174ae5d57057
Juan Silva <juan.silvawd@gmail.com>
2022-06-25 23:52:17 -0500

    naming

---
+++ src/PracticeOutofClass/NamingConventions.java
@@ -0,0 +1,17 @@
+package PracticeOutofClass;
+
+import java.util.Scanner;
+
+public class NamingConventions {
+
+    public static void main(String[] args) {
+        int a = 5;
+    }
+//
+//    public static int addTwoNumbers(int a, int b){
+//        return 0
+//    Scanner scan = new Scanner(System.in);
+    final int MAX_AGE = 100;
+
+    }
+
---
+++ src/PracticeOutofClass/TypeCasting.java
@@ -0,0 +1,20 @@
+package PracticeOutofClass;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+        int z = (int) 0.5;
+        System.out.println(z);
+// when casting a different type, the int changes .5 to round down to 0
+        byte a = 1;
+//        short b =;
+//        char c= ;
+//        int d = ;
+//        long e = ;
+//        float f = ;
+//        double g = ;
+
+        int ex = a;
+
+    }
+}
