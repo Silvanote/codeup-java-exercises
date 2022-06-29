15dc0aecb2d9fab31f42225fe06724e9270d6e00
Juan Silva <juan.silvawd@gmail.com>
2022-06-29 09:50:13 -0500

    another update

--- src/PracticeOutofClass/ContactList.java
+++ src/PracticeOutofClass/ContactList.java
@@ -17,12 +17,12 @@ public class ContactList {
 
         while (flag) {
             System.out.println("Welcome to Contract Manager App\n." +
-                    "1. View contacts." +
-                    "2. Add a new contact." +
-                    "3. Search a contact by name." +
-                    "4. Delete an existing contact." +
-                    "5. Exit." +
-                    "Enter an option (1, 2, 3, 4 or 5):");
+                    "1. View contacts.\n" +
+                    "2. Add a new contact.\n" +
+                    "3. Search a contact by name.\n" +
+                    "4. Delete an existing contact.\n" +
+                    "5. Exit.\n" +
+                    "Enter an option (1, 2, 3, 4 or 5):\n");
             int response = sc.nextInt();
             switch (response) {
                 case 1:
