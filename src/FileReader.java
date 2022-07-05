e7df740dffbc30fce22e34515732b7733c9745c3
Juan Silva <juan.silvawd@gmail.com>
2022-07-04 20:39:15 -0500

    add

---
+++ src/FileReader.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class FileReader {
+
+    public static void main(String[] args) {
+//http://textfiles.com/100/captmidn.txt
+        File file = new File("/Users/");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Give me a path");
+        scan.nextLine();
+
+    }
+
+}
